310dcb8ce41c6ae3b26787339087db19b6ebee04
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-10-10 09:39:24 +0000

    guesthouse画面作成

--- app/controllers/public/guesthouses_controller.rb
+++ app/controllers/public/guesthouses_controller.rb
@@ -1,13 +1,25 @@
 class Public::GuesthousesController < ApplicationController
   def index
+    @guesthouses = Guesthouse.all
   end
 
   def new
+    @guesthouse = Guesthouse.new
   end
 
   def create
+    @guesthouse = Guesthouse.new(guesthouse_params)
+    @guesthouse.save
+    redirect_to guesthouses_path, flash: { notice: '登録に成功しました' }
   end
 
   def show
   end
+
+  private
+
+  def guesthouse_params
+    params.require(:guesthouse).permit(:name, :address, :country_id, :customer_id)
+  end
+  
 end
--- app/models/guesthouse.rb
+++ app/models/guesthouse.rb
@@ -3,5 +3,6 @@ class Guesthouse < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to_active_hash :country
+  belongs_to :customer
 
 end
