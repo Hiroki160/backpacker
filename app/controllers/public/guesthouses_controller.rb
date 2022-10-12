a18a75daa8750cd48aa5c8f1ad21ff80de912aac
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-10-12 11:23:22 +0000

    fix
    -guesthouse一覧画面id指定

--- app/controllers/public/guesthouses_controller.rb
+++ app/controllers/public/guesthouses_controller.rb
@@ -1,6 +1,12 @@
 class Public::GuesthousesController < ApplicationController
   def index
-    @guesthouses = Guesthouse.all
+    # @guesthouses = Guesthouse.all
+    if params[:country_id]
+      @country = Country.find(params[:country_id])
+      @guesthouses = @country.guesthouses.all
+    else
+      @guesthouses = Guesthouse.all
+    end
   end
 
   def new
@@ -9,8 +15,10 @@ class Public::GuesthousesController < ApplicationController
 
   def create
     @guesthouse = Guesthouse.new(guesthouse_params)
+    @guesthouse.customer_id = current_customer.id
     @guesthouse.save
-    redirect_to guesthouses_path, flash: { notice: '登録に成功しました' }
+    redirect_to guesthouses_path(country_id: guesthouse_params[:country_id]), flash: { notice: '登録に成功しました' }
+    
     # 後からレビュー画面のパスに設定
   end
 
