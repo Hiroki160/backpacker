750415cb50a6a4d72f59e74ede0ca0ba49125b95
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-11-24 10:59:16 +0000

    申請機能作成

--- app/controllers/public/guesthouse_applies_controller.rb
+++ app/controllers/public/guesthouse_applies_controller.rb
@@ -1,11 +1,22 @@
 class Public::GuesthouseAppliesController < ApplicationController
   def create
     @guesthouse_apply = current_customer.guesthouse_applies.create(guesthouse_community_id: apply_params[:guesthouse_community_id])
+    @guesthouse_apply.customer_id = current_customer.id
     redirect_to guesthouse_community_guesthouse_apply_path(apply_params[:guesthouse_community_id], @guesthouse_apply.id), notice: "加入申請しました"
   end
 
   def show
-    @guesthous
+    @customer = current_customer
+    @guesthouse_applies = @customer.guesthouse_communities.all
+
+    if params[:guesthouse_apply][:status] == "accept"
+      @guesthouse_apply.status = 0
+    elsif params[:guesthouse_apply][:status] == "decline"
+      @guesthouse_apply.status = 1
+    else params[:guesthouse_apply][:status] == "waiting"
+      @guesthouse_apply.status = 2
+    end
+
   end
 
   def destroy
@@ -16,4 +27,5 @@ class Public::GuesthouseAppliesController < ApplicationController
   def apply_params
     params.permit(:guesthouse_community_id)
   end
+
 end
