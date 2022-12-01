450d0e8936dc0f02e9d8cafbdf2474063648b046
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-12-01 03:08:34 +0000

    レストランコミュニティ申請機能修正

--- app/controllers/public/restaurant_applies_controller.rb
+++ app/controllers/public/restaurant_applies_controller.rb
@@ -2,12 +2,27 @@ class Public::RestaurantAppliesController < ApplicationController
   def create
     @restaurant_apply = current_customer.restaurant_applies.create(restaurant_community_id: apply_params[:restaurant_community_id])
     @restaurant_apply.customer_id = current_customer.id
-    redirect_to restaurant_community_restaurant_apply_path(apply_params[:restaurante_community_id], @restaurant_apply.id), notice: "加入申請しました"
+    redirect_to restaurant_community_restaurant_apply_path(apply_params[:restaurant_community_id], @restaurant_apply.id), notice: "加入申請しました"
   end
 
   def destroy
   end
 
-  def show
+  def update
+    apply = RestaurantApply.find(params[:id])
+    apply.update(status: params[:restaurant_apply][:status])
+    redirect_to restaurant_community_path(apply.restaurant_community.id), notice: "更新しました！"
   end
+
+  def show
+    @customer = current_customer
+    @restaurant_applies = @customer.restaurant_applies
+  end
+
+  private
+
+  def apply_params
+    params.permit(:restaurant_community_id)
+  end
+
 end
