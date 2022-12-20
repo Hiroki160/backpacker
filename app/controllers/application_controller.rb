93741a3bd47b1edd5414d0e9f23eb4998ad96a86
EC2 Default User <sahiyuno0934@gmail.com>
2022-12-20 12:05:10 +0000

    背景画像レスポンシブ化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  
+
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_admin!, if: :admin_url
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name])
+  end
+
+  def admin_url
+    request.fullpath.include?("/admin")
   end
 
 end
